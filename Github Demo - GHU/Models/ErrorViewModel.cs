4469f790be2ab4642bc902fb7b313f41fe89b3ab
fiqriismail <fiqri@peachit.lk>
2019-11-21 19:22:10 +0530

    Add project files.

---
+++ Github Demo - GHU/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Github_Demo___GHU.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
